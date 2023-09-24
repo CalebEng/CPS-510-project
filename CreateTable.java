2b5e20b64eabb9b40b12e9b066a39e82ace62c0d
CalebEng <121829627+CalebEng@users.noreply.github.com>
2023-09-24 15:46:29 -0400

    fixed issue with code and streamlined table create

--- CreateTable.java
+++ CreateTable.java
@@ -45,11 +45,29 @@ public class CreateTable {
         passScanner.close();
 
         //try to connect to server and create file
-        
         Connection con = DriverManager.getConnection(oracleUrl, username, password);
         System.out.println("Connection successful!");
-
-        //creating the customer table
+        
+        //edit your needed createtion calls here. only call them once each. 
+        //comment the ones you dont need out.
+        //-------------------------------------------------------------------------
+        createCustomerTable(con);
+        createRoomTable(con);
+        createEmployeeTable(con);
+        createParkingSpotTable(con);
+        createAmenitiesTable(con);
+        createOnlineBookingTable(con);
+        createValetTable(con);
+        //-------------------------------------------------------------------------
+        
+        con.close();
+    }
+    /**
+     * Creates the table for customers. only call ONCE
+     * @param con
+     * @throws SQLException
+     */
+    private static void createCustomerTable(Connection con) throws SQLException{
         Statement stm = con.createStatement();
 
         String query = "create table CUSTOMER("
@@ -61,6 +79,102 @@ public class CreateTable {
             +"PRIMARY KEY (ID))";
         stm.execute(query);
         stm.close();
+        
         System.out.println("Customer table Created...");     
+    }
+
+    /**
+     * Creates the table for Rooms. only call ONCE
+     * @param con
+     * @throws SQLException
+     */
+    private static void createRoomTable(Connection con)throws SQLException{
+        Statement stm = con.createStatement();
+
+        String query = "create table ROOM(" 
+            + "ROOM_ID INT NOT NULL,"
+            + "ROOM_TYPE VARCHAR2(20) NOT NULL,"
+            + "CAPACITY INT NOT NULL,"
+            + "AVAILABILITY VARCHAR(20) DEFAULT 'Vacant',"
+            + "PRICE INT NOT NULL,"
+            + "PRIMARY KEY (ROOM_ID))";
+        stm.execute(query);
+        stm.close();
+        System.out.println("Room table Created...");   
+    }
+
+    /**
+     * Creates the table for Employees. only call ONCE
+     * @param con
+     * @throws SQLException
+     */
+    private static void createEmployeeTable(Connection con)throws SQLException{
+        Statement stm = con.createStatement();
+
+        String query = "create table EMPLOYEE(" 
+            + "E_ID INT NOT NULL,"
+            + "NAME VARCHAR(20) NOT NULL,"
+            + "PARTTIME_FULLTIME VARCHAR(20) NOT NULL,"
+            + "D_O_H date NOT NULL,"
+            + "PRIMARY KEY (E_ID))";
+        stm.execute(query);
+        stm.close();
+        System.out.println("Employee table Created..."); 
+    }
+
+    /**
+     * Creates 
+     * @param con
+     * @throws SQLException
+     */
+    private static void createParkingSpotTable(Connection con)throws SQLException{
+        Statement stm = con.createStatement();
+
+        String query = "create table PARKINGSPOT(" 
+            + "SPOT_NUMBER INT NOT NULL,"
+            + "AVALIABILITY VARCHAR(20) DEFAULT'Available' NOT NULL,"
+            + "PRIMARY KEY (SPOT_NUMBER))";
+        stm.execute(query);
+        stm.close();
+        System.out.println("Parking Spot table Created..."); 
+    }
+
+    /**
+     * Creates amenities table. only CALL ONCE
+     * @param con
+     * @throws SQLException
+     */
+    private static void createAmenitiesTable(Connection con)throws SQLException{
+        Statement stm = con.createStatement();
+
+        String query = "create table AMENITIES(" 
+            + "ID INT NOT NULL,"
+            + "GYM VARCHAR(20) NOT NULL,"
+            + "POOL VARCHAR(20) NOT NULL,"
+            + "ROOM_SERVICE VARCHAR(20) NOT NULL,"
+            + "BREAKFAST VARCHAR(20) NOT NULL,"
+            + "PRIMARY KEY (ID))";
+        stm.execute(query);
+        stm.close();
+        System.out.println("Amenities table Created..."); 
+    }
+
+    /**
+     * Creates online booking table. only call ONCE
+     * @param con
+     * @throws SQLException
+     */
+    private static void createOnlineBookingTable(Connection con)throws SQLException{
+
+    }
+
+
+    /**
+     * creates valet table. only call ONCE
+     * @param con
+     * @throws SQLException
+     */
+    private static void createValetTable(Connection con)throws SQLException{
+
     }
 }
